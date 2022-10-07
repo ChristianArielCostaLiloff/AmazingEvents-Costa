13c37b8c45bb70ce75594eb41dbc1c347f9ec7dc
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-10-06 22:26:08 -0300

    Task 2 finished

---
+++ assets/js/past-events.js
@@ -0,0 +1,27 @@
+const container = document.getElementById("main-content");
+
+for (let event of data.events) {
+    if (data.currentDate >  event.date) {
+      getCard(event);
+    }
+}
+
+function getCard(event) {
+    container.innerHTML += `
+    <article class="card">
+          <div class="card__info">
+            <figure>
+              <img src="${event.image}" alt="Image of ${event.name}" />
+            </figure>
+            <div class="card__title">
+              <h4>${event.name}</h4>
+              <p>${event.description}</p>
+            </div>
+          </div>
+            <div class="card__price">
+              <p>Price: $${event.price}</p>
+              <a href="#">Show more</a>
+            </div>
+        </article>
+  `;
+}
