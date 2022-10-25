a4c04afe5e15859163b5a441e15a13ca697f5ce3
ChristianArielCostaLiloff <private.c.costa@gmail.com>
2022-10-25 13:09:46 -0300

    fixed date in event

--- assets/js/event.js
+++ assets/js/event.js
@@ -17,6 +17,7 @@ async function showContent() {
   printEventInfo(data.event);
 }
 function printEventInfo(event) {
+  date = new Date(event.date).toDateString();
   container.innerHTML = `
     <article class="event">
       <figure>
@@ -29,12 +30,12 @@ function printEventInfo(event) {
         </div>
         <div>
           <div class="details-info">
-            <p>Date: ${event.date}</p>
+            <p>Date: ${date}</p>
             <p>Place: ${event.place}</p>
           </div>
           <div class="details-info">
             <p>
-              Joined:${event.assistance || event.estimate}/${event.capacity}
+              Joined: ${event.assistance || event.estimate}/${event.capacity}
             </p>
             <p>Price: $${event.price}</p>
           </div>
